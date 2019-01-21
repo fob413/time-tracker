0ca482940c684c53a24468338f2dea7aacf7e2f2
fob1493 <fob1493@gmail.com>
2019-01-21 12:53:24 +0100

    timer feature

--- App.js
+++ App.js
@@ -64,6 +64,29 @@ export default class App extends React.Component {
     });
   }
 
+  componentDidMount() {
+    const TIME_INTERVAL = 1000;
+
+    this.intervalId = setInterval(() => {
+      const { timers } = this.state;
+
+      this.setState({
+        timers: timers.map(timer => {
+          const { elapsed, isRunning } = timer;
+
+          return {
+            ...timer,
+            elapsed: isRunning ? elapsed + TIME_INTERVAL: elapsed,
+          };
+        }),
+      });
+    }, TIME_INTERVAL);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.intervalId);
+  }
+
   render() {
     const { timers } = this.state;
 
